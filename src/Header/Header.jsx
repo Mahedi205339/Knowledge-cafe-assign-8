a1abddf7a5f84a2dd3531f34a14c376197aa7020
mahedi205339 <mahedi205339gmaol.com>
2023-09-16 13:41:38 +0600

    added json fake data

---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+        <h1 className='text-4xl font-bold text-center my-10'>Cousre Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
